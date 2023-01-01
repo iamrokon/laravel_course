5f43c49bf1cbc888503653053794797e09d3019d
rokon <rokonuzzamancse@gmail.com>
2023-01-02 00:19:11 +0600

    class 12 done

--- app/Http/Controllers/CourseController.php
+++ app/Http/Controllers/CourseController.php
@@ -15,4 +15,29 @@ class CourseController extends Controller
         // }
         return view('course.single', compact('course'));
     }
+    public function courses(Request $request){
+        $search = $request->search;
+        $level = $request->level;
+        // if(!empty($search)){
+        //     $courses = Course::where('name','like','%'.$request->search.'%')->paginate(12);
+        // }else{
+        //     $courses = Course::paginate(12);
+        // }
+        $courses = Course::where(function ($query) use ($search) {
+            if(!empty($search)){
+                $query->where('name','like','%'.$search.'%');
+            }
+        })->when($level, function ($query) use ($level) {
+            if($level == 'beginner'){
+                $field = 0;
+            }elseif($level == 'intermediate'){
+                $field = 1;
+            }else{
+                $field = 2;
+            }
+            $query->where('difficulty_level',$field);
+        })->paginate(12);
+
+        return view('courses',compact('courses'));
+    }
 }
